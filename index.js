bd3b9bae0bb83ebae756a29640a0bd2b5355a6c5
sravankumar-samala <sravankumarsamala20@gmail.com>
2024-02-08 15:15:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { createPool } from 'mysql';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.APP_PORT
+const { DB_PORT, DB_HOST, MYSQL_DB, DB_USER, DB_PASS } = process.env
+console.log(DB_HOST, DB_PORT)
+
+const pool = createPool({
+    host: DB_HOST,
+    port: DB_PORT,
+    database: MYSQL_DB,
+    user: DB_USER,
+    password: DB_PASS,
+    connectionLimit: 10,
+})
+
+app.get('/', (req, res) => res.send('Hey there!'))
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
+
+app.get('/products', (req, res) => {
+    const sqlQuery = `SELECT * FROM roxilers_products;`
+
+    pool.query(sqlQuery, (err, result) => {
+        if (err) return console.log(err);
+        res.send(result);
+    })
+})
